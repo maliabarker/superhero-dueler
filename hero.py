8a1fc7366f79120a8aa291567b249b5d4446ef01
maliabarker <89502882+maliabarker@users.noreply.github.com>
2021-11-03 16:22:03 -0700

    added more methods to hero class

--- hero.py
+++ hero.py
@@ -1,42 +1,122 @@
 from os import name
+from ability import Ability
+from armor import Armor
 import random
 
 class Hero:
     def __init__(self, name, starting_health=100):
+        self.abilities = list()
+        self.armors = list()
+        
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
 
-    def add_ability(self, Ability):
-        pass
+    def add_ability(self, ability):
+        self.abilities.append(ability)
 
     def attack(self):
-        pass
+        total_damage = 0
+
+        for ability in self.abilities:
+            total_damage += ability.attack()
+
+        return total_damage
+
+    def add_armor(self, armor):
+        self.armors.append(armor)
 
     def defend(self, incoming_damage):
-        pass
+        for armor in self.armors:
+            incoming_damage -= armor.block()
+
+        return incoming_damage
 
     def take_damage(self, damage):
-        pass
+        remaining_damage = self.defend(damage)
+        if remaining_damage <= 0:
+            return f'No damage was taken. {self.name} has {self.current_health} health remaining.'
+        else:
+            self.current_health = self.current_health - remaining_damage
+            return f'{self.name} took {remaining_damage} points in damage. {self.name} has {self.current_health} health remaining.'
 
     def is_alive(self):
-        pass
+        if self.current_health <= 0:
+            return False
+        else:
+            return True
 
     def fight(self, opponent):
-        heros = [self.name, opponent.name]
-        winner = random.choice(heros)
-        if winner == heros[0]:
-            return f'{winner} defeats {heros[1]}'
-        elif winner == heros[1]:
-            return f'{winner} defeats {heros[0]}'
+        
+        if len(self.abilities) == 0 and len(opponent.abilities) == 0:
+            return 'Draw'
+        else:
+            while self.is_alive() == True and opponent.is_alive() == True:  
+                print(f'{self.name} and {opponent.name} attack!')
+                print(opponent.take_damage(self.attack()))
+                print(self.take_damage(opponent.attack()))
+                if self.is_alive() == True and opponent.is_alive() == False:
+                    return f'{self.name} defeated {opponent.name}!'
+                elif opponent.is_alive() == True and self.is_alive() == False:
+                    return f'{opponent.name} defeated {self.name}!'
+                elif self.is_alive() == False and opponent.is_alive() == False:
+                    return f'{self.name} and {opponent.name} defeated eachother. Draw!'
+                else:
+                    continue
+        
+        # heros = [self.name, opponent.name]
+        # winner = random.choice(heros)
+        # if winner == heros[0]:
+        #     return f'{winner} defeats {heros[1]}'
+        # elif winner == heros[1]:
+        #     return f'{winner} defeats {heros[0]}'
 
 
 
 if __name__ == "__main__":
   # If you run this file from the terminal
   # this block is executed.
-  my_hero = Hero("Grace Hopper", 200)
-  print(my_hero.name)
-  print(my_hero.current_health)
-  other_hero = Hero("Antman")
-  print(my_hero.fight(other_hero))
+  grace = Hero("Grace Hopper", 200)
+  print(grace.name)
+  print(grace.current_health)
+  antman = Hero("Antman")
+  print(grace.fight(antman))
+
+  debugging = Ability("Great Debugging", 50)
+  shock = Ability("Shock", 20)
+  grace.add_ability(debugging)
+  grace.add_ability(shock)
+  print(grace.abilities)
+  print(grace.attack())
+  iron_man_suit = Armor("Iron Man Suit", 50)
+  helmet = Armor("helmet", 15)
+  chestplate = Armor("chestplate", 30)
+  gloves = Armor("Super Gloves", 50)
+  grace.add_armor(gloves)
+  grace.add_armor(helmet)
+  print(grace.armors)
+#   print(grace.current_health)
+  print(grace.defend(100))
+  print(grace.take_damage(50))
+  print(grace.is_alive())
+  print(grace.take_damage(15000))
+  print(grace.is_alive())
+
+  print(len(grace.abilities))
+
+  superman = Hero("Superman", 200)
+  antman.add_ability(shock)
+
+  print(superman.fight(antman))
+
+  hero1 = Hero("Wonder Woman")
+  hero2 = Hero("Dumbledore")
+  ability1 = Ability("Super Speed", 300)
+  ability2 = Ability("Super Eyes", 130)
+  ability3 = Ability("Wizard Wand", 80)
+  ability4 = Ability("Wizard Beard", 20)
+  hero1.add_ability(ability1)
+  hero1.add_ability(ability2)
+  hero2.add_ability(ability3)
+  hero2.add_ability(ability4)
+  print(hero1.fight(hero2))
